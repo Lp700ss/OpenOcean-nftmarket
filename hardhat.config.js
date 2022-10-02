5bcbdefc09d4b99440237aa659e2868ea42c049d
Lp700s <siddhant@studio137.co.za>
2022-10-02 19:05:38 +0530

    nft contract

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "224fa98870494f6ba2088ca76b8aeb3e";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: "0.8.17",
+};
